027978d444259af4201f49f4c0c4e9df9d556213
harshit <harshitkumar9548@gmail.com>
2024-04-12 16:44:15 +0530

    Added client side logic

---
+++ react-ws/src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css'
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [socket,setSocket] = useState<null | WebSocket>(null);
+  const [latestMessage,setLatestMessage] = useState("");
+
+
+  useEffect(()=>{
+    const socket = new WebSocket('ws://localhost:8080');
+    setTimeout(socket.onopen = () =>{
+      console.log("connected");
+      setSocket(socket);
+    },5000);
+
+    socket.onmessage = (message) =>{
+      console.log("Recieved message: ",message.data);
+      setLatestMessage(message.data);
+    }
+   
+  },[])
+
+  if(!socket){
+    return(
+      <div>Connecting to the socket server loading...</div>
+    )
+  }
+  return (
+    <>
+    <h1>{latestMessage}</h1>
+    </>
+  )
+}
+
+export default App;
